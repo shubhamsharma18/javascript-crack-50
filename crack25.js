b406e628140e99856c65c31ad1b080ef1b1b3d62
Shubham <155475061+shubhamsharma18@users.noreply.github.com>
2025-06-26 12:23:09 +0000

    save

--- crack25.js
+++ crack25.js
@@ -1,11 +1,23 @@
-// Write a function that takes array and return first element of an array and also takes n parameter that return first n elements
-function returnarrelement(arr,n){
-    let num =arr[0]
-    console.log("First Element ",num);
+// Write a function that takes array and return first element of an array if n is not given and also takes n parameter that return first n elements
+function returnarrelement(arr, n) {
+
+    if (!n) {
+        let num = arr[0]
+        console.log("First Element ", num);
+    }
     
-   let result= arr.splice(0,n)
-   return result
+    else if (n < arr.length) {
+        let result = arr.slice(0, n)
+        console.log(result);
+        
+        
+    }
+    else {
+        console.log("enter valid no in given array");
+
+    }
+    
 }
-let arr=[1,2,3,4,5,6,7,8,9]
-let result=returnarrelement(arr,7)
-console.log(result);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ returnarrelement(arr,7)
+
